cf7e6eb8f2f3913288a23757b42e16e5f76f2496
srivardhanw <93868349+srivardhanw@users.noreply.github.com>
2023-03-09 08:35:51 +0530

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,13 +1,8 @@
 import pandas as pd
-import numpy as np
-from textblob import TextBlob
-from wordcloud import WordCloud
-import re
-import matplotlib.pyplot as plt
 import seaborn as sns
-plt.style.use('fivethirtyeight')
-import nltk
-from nltk.stem.snowball import SnowballStemmer
-from nltk.corpus import stopwords
-import spacy
-nlp = spacy.load("en_core_web_sm")
+import matplotlib.pyplot as plt
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+
+data = pd.read_csv("")
+print(data.head())
